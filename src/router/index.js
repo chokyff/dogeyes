c76a956cd21f9b6af2fbe66ed66d42f02874b727
chokyff <1053203848@qq.com>
2020-03-20 23:52:12 +0800

    首次提交

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import movieRouter from './Movie'
+import mineRouter from './Mine'
+import cinemaRouter from './Cinema'
+
+Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    movieRouter,
+    mineRouter,
+    cinemaRouter
+  ]
+})
+
+export default router
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello Cinema</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
